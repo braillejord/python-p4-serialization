0824c8e9fb833247895e44aa066d3c153e500ab6
BreElle Wells <breellejordyn@gmail.com>
2023-09-13 14:49:40 -0600

    use SQLAlchemy-Serializer to convert object into dictionary

--- server/models.py
+++ server/models.py
@@ -3,44 +3,56 @@ from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
 convention = {
-  "ix": "ix_%(column_0_label)s",
-  "uq": "uq_%(table_name)s_%(column_0_name)s",
-  "ck": "ck_%(table_name)s_%(constraint_name)s",
-  "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-  "pk": "pk_%(table_name)s"
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
 }
 
 metadata = MetaData(naming_convention=convention)
 
 db = SQLAlchemy(metadata=metadata)
 
-class Zookeeper(db.Model):
-    __tablename__ = 'zookeepers'
+
+class Zookeeper(db.Model, SerializerMixin):
+    __tablename__ = "zookeepers"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
     birthday = db.Column(db.Date)
 
-    animals = db.relationship('Animal', backref='zookeeper')
+    animals = db.relationship("Animal", backref="zookeeper")
 
-class Enclosure(db.Model):
-    __tablename__ = 'enclosures'
+    serialize_rules = ("-animals.zookeeper",)
+
+
+class Enclosure(db.Model, SerializerMixin):
+    __tablename__ = "enclosures"
 
     id = db.Column(db.Integer, primary_key=True)
     environment = db.Column(db.String)
     open_to_visitors = db.Column(db.Boolean)
 
-    animals = db.relationship('Animal', backref='enclosure')
+    animals = db.relationship("Animal", backref="enclosure")
 
-class Animal(db.Model):
-    __tablename__ = 'animals'
+    serialize_rules = ("-animals.enclosure",)
+
+
+class Animal(db.Model, SerializerMixin):
+    __tablename__ = "animals"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, unique=True)
     species = db.Column(db.String)
 
-    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
-    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey("zookeepers.id"))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey("enclosures.id"))
+
+    serialize_rules = (
+        "-zookeeper.animals",
+        "-enclosure.animals",
+    )
 
     def __repr__(self):
-        return f'<Animal {self.name}, a {self.species}>'
+        return f"<Animal {self.name}, a {self.species}>"
